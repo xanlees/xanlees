215634ef2188a848318fe567362a1cd0e635ef2b
Thavisouk MNLV <Thavisoukmnlv@gmail.com>
2023-12-30 14:52:11 +0700

    commit code Sat 30

---
+++ src/app/(protected)/employee/stories/show.stories.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Show from "../page";
+
+export default {
+  title: "employee/show",
+  component: Show,
+  parameters: {
+    nextjs: {
+      appDirectory: true,
+      navigation: {
+        pathname: "/employee",
+      },
+    },
+  },
+};
+
+export const show = () => <Show />;
