0bdf9df065560a0e9eb271734a1e498400b4c119
Thavisouk MNLV <Thavisoukmnlv@gmail.com>
2023-12-30 13:46:41 +0700

    commit code Fri 29

--- src/app/(protected)/user/components/form.tsx
+++ src/app/(protected)/user/components/form.tsx
@@ -1,30 +1,22 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable max-lines */
 /* eslint-disable max-lines-per-function */
+/* eslint-disable @typescript-eslint/naming-convention */
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useSelect, type RedirectAction } from "@refinedev/core";
 import { useForm } from "@refinedev/react-hook-form";
-import * as z from "zod";
+import type * as z from "zod";
 import { Input } from "@/shadcn/ui";
 import { Form } from "@/shadcn/components/form";
 import { type IGroup } from "../interface";
+import { userSchema } from "../validation/validation";
 
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-  isActive: z.union([z.string(), z.boolean()]),
-  groups: z.array(z.string()),
-});
-
-// eslint-disable-next-line @typescript-eslint/naming-convention
 export const UserForm = ({
   redirect = "edit",
 }: {
   redirect: RedirectAction
 }) => {
-  const { ...form } = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const { ...form } = useForm<z.infer<typeof userSchema>>({
+    resolver: zodResolver(userSchema),
     defaultValues: {
       username: "",
       isActive: false,
@@ -38,6 +30,7 @@ export const UserForm = ({
     },
     warnWhenUnsavedChanges: true,
   });
+
   const groups = useSelect<IGroup>({
     resource: "group",
     optionLabel: "name",
@@ -45,35 +38,39 @@ export const UserForm = ({
   });
 
   return (
-    <>
-      <Form {...form}>
-        <Form.Field {...form} name="username" label="Username">
-          <Input placeholder="Username" />
+    <Form {...form}>
+      <Form.Field {...form} name="username" label="Username">
+        <Input placeholder="Username" />
+      </Form.Field>
+      <Form.Field {...form} name="password" label="Password">
+        <Input placeholder="Password" />
+      </Form.Field>
+      <div className="inline-flex flex-row items-center justify-start gap-x-4">
+        <Form.Field {...form} name="groups" label="Group">
+          <Form.Combobox
+            {...(groups as any)}
+            onChange={(value) => {
+              form.setValue("groups", [value]);
+            }}
+          />
         </Form.Field>
-        <div className="inline-flex flex-row items-center justify-start gap-x-4">
-          <Form.Field {...form} name="groups" label="Group">
-            <Form.Combobox {...groups as any}
-              onChange={(value) => {
-                form.setValue("groups", [value]);
-              }}
-            />
-          </Form.Field>
-          <Form.Field {...form} name="isActive" label="Status">
-            <Form.Select
-              options={[
-                {
-                  label: "ເປີດໃຊ້ງານ",
-                  value: "true",
-                },
-                {
-                  label: "ປີດໃຊ້ງານ",
-                  value: "false",
-                },
-              ]}
-            />
-          </Form.Field>
-        </div>
-      </Form>
-    </>
+        <Form.Field {...form} name="isActive" label="Status">
+          <Form.Select
+            options={userStatus}
+          />
+        </Form.Field>
+      </div>
+    </Form>
   );
 };
+
+const userStatus = [
+  {
+    label: "ເປີດໃຊ້ງານ",
+    value: "true",
+  },
+  {
+    label: "ປີດໃຊ້ງານ",
+    value: "false",
+  },
+];
