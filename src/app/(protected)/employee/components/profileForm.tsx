cd4c1f54790a9da4f49ee0e423f91729bc9a9830
bui <silivanxay@gmail.com>
2023-12-31 17:26:07 +0700

    update datepicker component

--- src/app/(protected)/employee/components/profileForm.tsx
+++ src/app/(protected)/employee/components/profileForm.tsx
@@ -11,6 +11,7 @@ import { genderOptions, maritalStatusOptions } from "../lib/constant";
 import { profileSchema } from "../validation/validation";
 import { useCounter } from "./context";
 import { Form } from "@src/shadcn/components/form";
+import { DatePickerField } from "@src/shadcn/components/form/datepicker2";
 
 interface ProfileFormProps {
   redirect: RedirectAction
@@ -51,28 +52,28 @@ export const ProfileForm: React.FC<ProfileFormProps> = ({ redirect }) => {
   return (
     <div className="w-[39%] rounded-full ">
       <Form {...form}>
-        <div className="flex flex-col w-full capitalize rounded-lg sm:w-1/2 sm:flex-row">
-          <div className="flex-1 p-4">
-            <Form.Field {...form} name="fullname" label="fullname">
-              <Input placeholder="Fullname" />
-            </Form.Field>
-            <Form.Field {...form} name="phoneNumber" label="Phone Number">
-              <Input placeholder="Phone Number" />
-            </Form.Field>
-            <Form.Field {...form} name="gender" label="Gender">
-              <Form.Select options={genderOptions} />
-            </Form.Field>
-            <Form.FileInput label={"Profile"} {...form} name="profilePicture" />
-          </div>
-          <div className="flex-1 p-4 ">
-            <Form.Field {...form} name="nickname" label="nickname">
-              <Input placeholder="Nickname" />
-            </Form.Field>
-            <Form.DatePicker {...form} name="birthday" label="Date of Birth" />
-            <Form.Field {...form} name="maritalStatus" label="Marital Status">
-              <Form.Select options={maritalStatusOptions} />
-            </Form.Field>
-          </div>
+        <div className="flex-1 p-4">
+          <Form.Field {...form} name="fullname" label="fullname">
+            <Input placeholder="Fullname" />
+          </Form.Field>
+          <Form.Field {...form} name="phoneNumber" label="Phone Number">
+            <Input placeholder="Phone Number" />
+          </Form.Field>
+          <Form.Field {...form} name="gender" label="Gender" >
+            <Form.Select options={genderOptions} />
+          </Form.Field>
+          <Form.FileInput label={"Profile"} {...form} name="profilePicture" />
+        </div>
+        <div className="flex-1 p-4 ">
+          <Form.Field {...form} name="nickname" label="nickname">
+            <Input placeholder="Nickname" />
+          </Form.Field>
+          <Form.Field {...form} name="birthday" label="Date of Birth">
+            <DatePickerField />
+          </Form.Field>
+          <Form.Field {...form} name="maritalStatus" label="Marital Status">
+            <Form.Select options={maritalStatusOptions} />
+          </Form.Field>
         </div>
       </Form>
     </div>
