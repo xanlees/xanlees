2003da73685960d32812c86004452a2152f9546d
Thavisouk MNLV <Thavisoukmnlv@gmail.com>
2024-01-02 13:29:41 +0700

    add profle

--- src/app/(protected)/(career)/employee/create/page.tsx
+++ src/app/(protected)/(career)/employee/create/page.tsx
@@ -1,27 +1,13 @@
 "use client";
 
+import React from "react";
+import { EmployeeForm } from "../components/form";
 import { Create } from "@/shadcn/components/crud";
-import { EmployeeProvider } from "../../context/context";
-import { createEmployeeSteps } from "../lib/settings";
-import { useState } from "react";
-import Stepper from "@keyvaluesystems/react-stepper";
 
 export default function EmployeeCreate(): JSX.Element {
-  const [currentStep, setCurrentStep] = useState(0);
-  const handleStepClick = (_: any, index: number) => {
-    setCurrentStep(index);
-  };
   return (
     <Create>
-      <EmployeeProvider>
-        <Stepper
-          steps={createEmployeeSteps}
-          orientation="vertical"
-          currentStepIndex={currentStep}
-          onStepClick={handleStepClick}
-          stepContent={() => <div className="w-[1600px] rounded-lg" />}
-        />
-      </EmployeeProvider>
+      <EmployeeForm redirect="list" />
     </Create>
   );
 }
--- src/app/(protected)/(career)/employee/lib/settings.tsx
+++ src/app/(protected)/(career)/employee/lib/settings.tsx
@@ -1,30 +1,6 @@
-import { PersonalAddressForm } from "@src/app/(protected)/(personal)/address/components/form";
-import { EducationForm } from "@src/app/(protected)/(personal)/education/components/form";
-import { GraduationForm } from "@src/app/(protected)/(personal)/graduation/components/form";
-import { ProfileForm } from "@src/app/(protected)/(personal)/profile/components/form";
-import { EmployeeForm } from "../components/employeeForm";
+import { EmployeeForm } from "../components/form";
 
 export const createEmployeeSteps = [
-  {
-    stepLabel: "Personal Address",
-    stepDescription: <PersonalAddressForm redirect="create" />,
-    completed: false,
-  },
-  {
-    stepLabel: "Creating Profile",
-    stepDescription: <ProfileForm redirect="create" />,
-    completed: false,
-  },
-  {
-    stepLabel: "Create Graduation",
-    stepDescription: <GraduationForm redirect="create" />,
-    completed: false,
-  },
-  {
-    stepLabel: "Create Education",
-    stepDescription: <EducationForm redirect="create" />,
-    completed: false,
-  },
   {
     stepLabel: "Create Employee",
     stepDescription: <EmployeeForm redirect="create" />,
