758a94b93acbe9b273a624a3ca88a2036b3d6c4c
Thavisouk MNLV <Thavisoukmnlv@gmail.com>
2024-01-21 00:58:43 +0700

    fix layout profile form

--- src/app/(protected)/(personal)/document/components/form/RenderFile.tsx
+++ src/app/(protected)/(personal)/document/components/form/RenderFile.tsx
@@ -4,6 +4,7 @@ import { Button } from "@src/shadcn/elements";
 
 interface DocumentFile {
   size: number
+  name: string
 }
 
 interface RenderFileProps {
@@ -18,23 +19,25 @@ interface RenderFileProps {
 const bytesToKbConversionFactor = 1024;
 
 const RenderFile: React.FC<RenderFileProps> = ({ field, removeField }) => {
-  let fileSize = "NaN";
+  let fileSize = "ບໍ່ມີຂໍ້ມູນ";
+  let fileName = "";
   if (field.documentFile instanceof FileList && field?.documentFile?.length > 0) {
     const selectedFile = field?.documentFile[0];
     const fileSizeInMB = (selectedFile?.size / (bytesToKbConversionFactor * bytesToKbConversionFactor)).toFixed(2);
     fileSize = `${fileSizeInMB} MB`;
+    fileName = selectedFile?.name ?? "";
   }
   return (
-    <div className="flex justify-between p-2 border rounded-md border-input" key={field.id}>
+    <div className="flex items-center justify-between p-4 border rounded-md border-input" key={field.id}>
       <div className="flex items-center">
-        <FileText className="w-9 h-9" />
-        <div className="flex flex-col ml-2">
-          <div className="text-lg">{field?.documentName}</div>
-          <div className="text-sm text-blue-500">{`File size: ${fileSize}`}</div>
+        <FileText className="w-12 h-12 text-blue-500" />
+        <div className="flex flex-col ml-4">
+          <div className="text-xl font-semibold text-gray-800">{field?.documentName}</div>
+          <div className="text-sm text-blue-500">{`${fileName}:`} <span className="text-black">{fileSize}</span></div>
         </div>
       </div>
-      <Button onClick={removeField} className="bg-red-500">
-        <Trash2 />
+      <Button onClick={removeField} className="p-2 bg-red-500 rounded">
+        <Trash2 className="w-6 h-6 text-white" />
       </Button>
     </div>
   );
