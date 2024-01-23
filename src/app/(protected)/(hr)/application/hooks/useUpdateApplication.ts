fe1426e9e748be3c655a122c82dd2ea82e12f97f
Thavisouk MNLV <Thavisoukmnlv@gmail.com>
2024-01-23 12:07:27 +0700

    EM-99 <message>

--- src/app/(protected)/(hr)/application/hooks/useUpdateApplication.ts
+++ src/app/(protected)/(hr)/application/hooks/useUpdateApplication.ts
@@ -1,5 +1,4 @@
 import { useUpdate } from "@refinedev/core";
-
 export const useUpdateApplication = () => {
   const { mutate } = useUpdate();
   const updateApplication = ({ id, value }: { id: number, value: string }) => {
