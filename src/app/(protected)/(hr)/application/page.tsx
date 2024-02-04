259bd533b52054bec7f28b80b1c1e46c30a02cdc
Thavisouk MNLV <Thavisoukmnlv@gmail.com>
2024-02-04 17:24:10 +0700

    EM-96

--- src/app/(protected)/(hr)/application/page.tsx
+++ src/app/(protected)/(hr)/application/page.tsx
@@ -6,43 +6,22 @@ import { useUserFriendlyName } from "@refinedev/core";
 import { useTable } from "@refinedev/react-table";
 import { getActionsColumn } from "@src/common/containers/column/action";
 import { getSelectColumn } from "@src/common/containers/column/select";
-import {
-  ApplicationDate,
-  ApplicationStatusColumn,
-  workExperienceColumn,
-  AppliedPosition,
-  ExpectedSalary,
-} from "./containers/column";
+import { ApplicationDate, ApplicationStatusColumn, workExperienceColumn, AppliedPosition, ExpectedSalary } from "./containers/column";
 import { useApplication, useApplicationID } from "./hooks";
 import type { IApplication } from "./interface";
 import { application_resource, refineCoreProps } from "./lib/constant";
-import {
-  GenderColumn,
-  FullNameColumn,
-  PhoneNumberColumn,
-  MarriageColumn,
-} from "@src/common/containers/column";
+import { GenderColumn, FullNameColumn, PhoneNumberColumn, MarriageColumn, } from "@src/common/containers/column";
 
 export default function ApplicationList(): JSX.Element {
-  const table = useTable<IApplication>({
-    columns: [],
-    enableSorting: true,
-    enableColumnFilters: true,
-    refineCoreProps,
-  });
+  const table = useTable<IApplication>({ columns: [], enableSorting: true, enableColumnFilters: true, refineCoreProps });
   const application = table.options.data ?? [];
   const applicationID = useApplicationID(application);
-  const { data: dataWorkExperience } = useApplication({
-    applicationID,
-    application,
-  });
+  const { data: dataWorkExperience } = useApplication({ applicationID, application });
   const friendly = useUserFriendlyName();
   return (
     <List showCreate={false}>
       <span className="mx-2 mt-10 text-sm font-bold">ສົ່ງລີ້ງໃຫ້ຜູ້ສະໝັກ:</span>
-      <InputToClipboard
-        value={`${process.env.NEXTAUTH_URL}/application/create`}
-      ></InputToClipboard>
+      <InputToClipboard value={`${process.env.NEXTAUTH_URL}/application/create`} />
       <div className="-mt-[3.25rem]">
         <Table table={table}>
           {getSelectColumn(friendly)}
