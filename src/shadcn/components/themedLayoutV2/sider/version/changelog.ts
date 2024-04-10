2f635fe70cab8e7b995dfbcff8639824b4dab7fe
bui <silivanxay@gmail.com>
2024-04-10 12:32:25 +0700

    v0.4.0

--- src/shadcn/components/themedLayoutV2/sider/version/changelog.ts
+++ src/shadcn/components/themedLayoutV2/sider/version/changelog.ts
@@ -1,16 +1,6 @@
 export const changelogData = [{
-    version: "Version: 0.1.2",
+    version: "Version: 0.4.0",
     features: [
-        {
-            id: 1,
-            title: "ເພີ່ມເອກະສານ",
-            badge: "ໃໝ່"
-        },
-        {
-            id: 2,
-            title: "ຟອມສະໝັກວຽກ",
-            badge: "ໃໝ່"
-        }
     ]
 }
 ];
